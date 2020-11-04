cb9dca351e6bab60ddf2ca2476bce9aa00a2762e
unknown <nesk_woe@hotmail.com>
2020-11-04 12:45:04 +0800

    test

--- hello_world/main.py
+++ hello_world/main.py
@@ -12,18 +12,20 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import io
+import os
+import logging
+import cloudstorage as gcs
+from google.cloud import speech
+
 # [START gae_python38_app]
 def run_quickstart():
     # [START speech_quickstart]
-    import io
-    import os
-    import logging
-    import cloudstorage as gcs
-    import webapp2
-    
+
+
     # Imports the Google Cloud client library
     # [START speech_python_migration_imports]
-    from google.cloud import speech
+    
 
     # [END speech_python_migration_imports]
 
@@ -53,5 +55,44 @@ def run_quickstart():
         print("Transcript: {}".format(result.alternatives[0].transcript))
     # [END speech_quickstart]
 
+def transcribe_gcs(gcs_uri):
+    """Asynchronously transcribes the audio file specified by the gcs_uri."""
+    from google.cloud import speech
+
+    client = speech.SpeechClient()
+
+    audio = speech.RecognitionAudio(uri=gcs_uri)
+    config = speech.RecognitionConfig(
+        encoding=speech.RecognitionConfig.AudioEncoding.FLAC,
+        sample_rate_hertz=16000,
+        language_code="en-US",
+    )
+
+    operation = client.long_running_recognize(
+        request={"config": config, "audio": audio}
+    )
+
+    operation = client.long_running_recognize(config=config, audio=audio)
+
+    print("Waiting for operation to complete...")
+    response = operation.result(timeout=90)
+
+    # Each result is for a consecutive portion of the audio. Iterate through
+    # them to get the transcripts for the entire audio file.
+    for result in response.results:
+        # The first alternative is the most likely one for this portion.
+        print(u"Transcript: {}".format(result.alternatives[0].transcript))
+        print("Confidence: {}".format(result.alternatives[0].confidence))
+
+def read_file(self, filename):
+  self.response.write('Reading the full file contents:\n')
+
+  gcs_file = gcs.open(filename)
+  contents = gcs_file.read()
+  gcs_file.close()
+  self.response.write(contents)
+
+
 if __name__ == "__main__":
-    run_quickstart()
+    #run_quickstart()
+    transcribe_gcs('gs://eep-m22-team5/audiofile/test1.MP3')
